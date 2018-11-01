779ca91ee4c1b764a59112fac42aa0728394fc56
Pedro Henrique <pedro00dk@gmail.com>
2018-11-01 11:36:15 -0300

    Use http.handle that already provide an URL mux

--- src/main.go
+++ src/main.go
@@ -3,23 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func main() {
-	server := &http.Server{
-		Addr:           ":8000",
-		Handler:        Handler{},
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-	server.ListenAndServe()
+	// server := &http.Server{
+	// 	Addr:           ":8000",
+	// 	Handler:        Handler{},
+	// 	ReadTimeout:    10 * time.Second,
+	// 	WriteTimeout:   10 * time.Second,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
+	// server.ListenAndServe()
+
+	http.HandleFunc("/functions", functions)
+	http.ListenAndServe(":8000", nil)
 }
 
 // Handler represents the http struct that hold a function to process requests.
-type Handler struct{}
+// type Handler struct{}
 
-func (h Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte(fmt.Sprintf("[%v] %v", req.Method, req.RequestURI)))
+// func (h Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+// 	res.Write([]byte(fmt.Sprintf("[%v] %v", req.Method, req.RequestURI)))
+// }
+
+func functions(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte(fmt.Sprintf("hello [%v] %v", req.Method, req.RequestURI)))
 }
